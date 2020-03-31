d468e8361e040a8268c8df19f9a2a6ebf53f59d8
Dennis S.Otavio <39060358+denisotavio1992@users.noreply.github.com>
2020-03-30 21:02:30 -0300

    vender 10%
    ...

--- login/src/app/tasks/pages/vender/vender.page.ts
+++ login/src/app/tasks/pages/vender/vender.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../../models/produto.model';
+import { NavController } from '@ionic/angular';
+import { OverlayService } from 'src/app/core/services/overlay.service';
+import { ProdutosService } from '../../services/produtos.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-vender',
   templateUrl: './vender.page.html',
-  styleUrls: ['./vender.page.scss'],
+  styleUrls: ['./vender.page.scss']
 })
 export class VenderPage implements OnInit {
+  produtos$: Observable<Produto[]>;
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private overlayService: OverlayService,
+    private produtosService: ProdutosService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit(): Promise<void> {
+    const loading = await this.overlayService.loading();
+    this.produtos$ = this.produtosService.getAll();
+    this.produtos$.pipe(take(1)).subscribe(produtos => loading.dismiss());
   }
-
 }
